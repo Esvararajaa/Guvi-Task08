ab6d1cee8414b688af48f76a2448ad084db9f992
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2023-10-03 08:01:59 +0530

    Add files via upload

---
+++ Area_Perimeter.py
@@ -0,0 +1,17 @@
+class Circle():
+    _pi=3.14
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2*Circle._pi
+    
+    def perimeter(self):
+        return 2*self.radius*Circle._pi
+    
+ls = [10, 501, 22, 37, 100, 999, 87, 351]
+for n in ls:
+    NewCircle = Circle(n)
+    print(f"Area of circle with {n}: {NewCircle.area()}")
+    print(f"Perimeter of circle with {n}: {NewCircle.perimeter()}")
+

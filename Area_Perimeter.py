69f25c99a01789bebead0c79a1fb46d9279f7d70
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2023-10-03 08:07:43 +0530

    Update Area_Perimeter.py

--- Area_Perimeter.py
+++ Area_Perimeter.py
@@ -1,13 +1,13 @@
 class Circle():
-    _pi=3.14
+    __pi=3.14
     def __init__(self, r):
         self.radius = r
 
     def area(self):
-        return self.radius**2*Circle._pi
+        return self.radius**2*Circle.__pi
     
     def perimeter(self):
-        return 2*self.radius*Circle._pi
+        return 2*self.radius*Circle.__pi
     
 ls = [10, 501, 22, 37, 100, 999, 87, 351]
 for n in ls:
